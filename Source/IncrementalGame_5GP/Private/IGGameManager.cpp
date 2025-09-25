cd15cfaf73d8948c781dbaec9c1aebc1cbf747aa
Nyukiland <roman211113@gmail.com>
2025-09-26 01:30:17 +0200

    [CLEAN] Manager and Enemy

--- Source/IncrementalGame_5GP/Private/IGGameManager.cpp
+++ Source/IncrementalGame_5GP/Private/IGGameManager.cpp
@@ -1,29 +1,8 @@
 #include "IGGameManager.h"
-#include "IGEnemy.h"
 
 void UIGGameManager::Tick(float DeltaTime)
 {
-	Timer += DeltaTime;
-
-	if (Timer >= CurrentEnemySpawnRate)
-	{
-		Timer = 0;
-		SpawnEnemy();
-	}
-}
-
-void UIGGameManager::EnemyWentInactive(AIGEnemy* Enemy)
-{
-	int Index = FindEnemyIndexInList(Enemy);
-
-	if (Index == -1)
-	{
-		UE_LOG(LogTemp, Error, TEXT("[Game Manager] Index of found Enemy is not valid"));
-		return;
-	}
-
-	Enemies.Swap(Index, CurrentEnemiesActive-1);
-	CurrentEnemiesActive--;
+	
 }
 
 void UIGGameManager::OnWorldBeginPlay(UWorld& InWorld)
@@ -31,63 +10,22 @@ void UIGGameManager::OnWorldBeginPlay(UWorld& InWorld)
 	Super::OnWorldBeginPlay(InWorld);
 }
 
-void UIGGameManager::SpawnEnemy()
-{
-	if (!EnemyToSpawn)
-	{
-		UE_LOG(LogTemp, Error, TEXT("[GameManager] EnemyToSpawn is null, can't spawn"));
-		return;
-	}
-
-	if (CurrentEnemiesActive >= CurrentMaxEnemyCount)
-		return;
-
-	if (Enemies.Num() <= CurrentEnemiesActive)
-	{
-		AIGEnemy* Enemy = GetWorld()->SpawnActor<AIGEnemy>(EnemyToSpawn);
-		Enemy->InitEnemy(this);
-		Enemies.Add(Enemy);
-	}
-
-	FVector Temp = FVector::ZeroVector;
-	Enemies[CurrentEnemiesActive]->EnableEnemy(Temp, Temp, 0);
-	CurrentEnemiesActive++;
-}
-
-int UIGGameManager::FindEnemyIndexInList(AIGEnemy* Enemy, bool bFindInAll, bool bClampToActive)
-{
-	if (!Enemy)
-	{
-		UE_LOG(LogTemp, Error, TEXT("[GameManager] Given Enemy to find is invalid"));
-		return -1;
-	}
-	
-	int StartIndex = 0;
-	int EndIndex = Enemies.Num();
-	
-	if (bFindInAll && bClampToActive)
-		EndIndex = CurrentEnemiesActive;
-	else if (!bFindInAll)
-		StartIndex = CurrentEnemiesActive;
-
-	if (Enemies.Num() <= StartIndex)
-	{
-		UE_LOG(LogTemp, Error, TEXT("[GameManager] Start Index is bigger than Enemies count"))
-		return -1;
-	}
-
-	for (int i = StartIndex; i < EndIndex; i++)
-	{
-		if (!Enemies[i]) continue;
-		
-		if (Enemies[i] == Enemy) return i;
-	}
-
-	UE_LOG(LogTemp, Error, TEXT("[GameManager] Enemy couldn't be found"))
-	return -1;
-}
-
 TStatId UIGGameManager::GetStatId() const
 {
 	RETURN_QUICK_DECLARE_CYCLE_STAT(UIGGameManager, STATGROUP_Tickables);
+}
+
+int UIGGameManager::GetFursthestEnemy()
+{
+	return 0;
+}
+
+int UIGGameManager::GetClosestEnemy()
+{
+	return 0;
+}
+
+int UIGGameManager::GetRandomEnemy()
+{
+	return 0;
 }
--- Source/IncrementalGame_5GP/Public/IGGameManager.h
+++ Source/IncrementalGame_5GP/Public/IGGameManager.h
@@ -4,20 +4,40 @@
 #include "Subsystems/WorldSubsystem.h"
 #include "IGGameManager.generated.h"
 
-class AIGEnemy;
 class UIGMathEquations;
+
+USTRUCT(BlueprintType)
+struct FEnemyData //can create with a simple var "FEnemyData Enemy"
+{
+	GENERATED_BODY()
+
+private:
+	float Life = 0;
+
+public:
+	void ApplyDamage(float Damage)
+	{
+		if (Life <= 0)
+			return;
+		
+		Life -= Damage;
+		if (Life <= 0) Death();
+	}
+
+	void Death()
+	{
+		
+	}
+};
 
 UCLASS(Blueprintable)
 class INCREMENTALGAME_5GP_API UIGGameManager : public UWorldSubsystem, public FTickableGameObject
 {
 	GENERATED_BODY()
-	
+
 protected:
 	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Enemy")
 	TObjectPtr<UIGMathEquations> MaxSpawnCountCurve;
-
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Enemy")
-	TSubclassOf<AIGEnemy> EnemyToSpawn;
 
 	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Enemy")
 	TObjectPtr<UIGMathEquations> SpawnRateCurve;
@@ -28,26 +48,15 @@ protected:
 	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Enemy")
 	float CurrentEnemySpawnRate;
 
-	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Enemy")
-	TArray<TObjectPtr<AIGEnemy>> Enemies;
-
-	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Enemy")
-	int CurrentEnemiesActive;
-	
-	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Enemy")
-	float Timer;
-
-public:	
+public:
 	virtual void Tick(float DeltaTime) override;
-	void EnemyWentInactive(AIGEnemy* Enemy);
 	virtual bool IsTickable() const override { return true; }
 	virtual TStatId GetStatId() const override;
 
-	
+	int GetFursthestEnemy();
+	int GetClosestEnemy();
+	int GetRandomEnemy();
+
 protected:
 	virtual void OnWorldBeginPlay(UWorld& InWorld) override;
-
-private:
-	void SpawnEnemy();
-	int FindEnemyIndexInList(AIGEnemy* Enemy, bool bFindInAll = false, bool bClampToActive = false);
 };
